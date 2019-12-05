94622a088f5d6e12d5647d6961f0fce8b20fc1fb
dicky Ardianto <dikiardianto19@gmail.com>
2019-12-05 22:09:36 +0700

    Berhasil menambahkan fitur edit dan delete pada laravel

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -87,7 +87,7 @@ class StudentsController extends Controller
      */
     public function edit(Student $student)
     {
-        //
+        return view('students.edit', compact('student'));
     }
 
     /**
@@ -99,7 +99,19 @@ class StudentsController extends Controller
      */
     public function update(Request $request, Student $student)
     {
-        //
+        $request->validate([
+            'nama' => 'required',
+            'nrp' => 'required|size:9'
+        ]);
+
+        Student::where('id', $student->id)->update([
+            'nama' => $request->nama,
+            'nrp' => $request->nrp,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan
+        ]);
+        return redirect('/students')->with('status', 'Data Mahasiswa berhasil diubah');
+        // return $student;
     }
 
     /**
@@ -110,6 +122,8 @@ class StudentsController extends Controller
      */
     public function destroy(Student $student)
     {
+        Student::destroy($student->id);
+        return redirect('/students')->with('status', 'Data Mahasiswa berhasil dihapus');
         //
     }
 }
--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -5,33 +5,34 @@
 <div class="container">
     <div class="row">
         <div class="col-8">
-            <h1 class="mt-4">Form tambah data Mahasiswa</h1>
+            <h1 class="mt-4">Form ubah data Mahasiswa</h1>
 
-            <form method="post" action="/students">
+            <form method="post" action="/students/{{$student->id}}">
+                @method('patch')
                 @csrf
                 <div class="form-group">
                     <label for="nama">Nama</label>
                     <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama"
-                        placeholder="Masukkan nama" value="{{old('nama')}}">
+                        placeholder="Masukkan nama" value="{{$student->nama}}">
                     @error('nama')<div class="invalid-feedback">{{ $message }}</div>@enderror
                 </div>
                 <div class="form-group">
                     <label for="nrp">NRP</label>
                     <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" name="nrp"
-                        placeholder="Masukkan Nrp" value="{{old('nrp')}}">
+                        placeholder="Masukkan Nrp" value="{{$student->nrp}}">
                     @error('nrp')<div class="invalid-feedback">{{ $message }}</div>@enderror
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="text" class="form-control" id="email" name="email" placeholder="Masukkan email"
-                        value="{{old('email')}}">
+                        value="{{$student->email}}">
                 </div>
                 <div class="form-group">
                     <label for="jurusan">Jurusan</label>
                     <input type="text" class="form-control" id="jurusan" name="jurusan" placeholder="Masukkan jurusan"
-                        value="{{old('jurusan')}}">
+                        value="{{$student->jurusan}}">
                 </div>
-                <button type="submit" class="btn btn-primary">Tambah</button>
+                <button type="submit" class="btn btn-primary">Ubah data</button>
             </form>
 
         </div>
